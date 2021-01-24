36cc60f11ab96e738830bb69a0ad9c1d8dd07090
Niklas Sauter <niklas@n1ks.net>
2021-01-24 03:59:44 +0100

    Update error messages

--- src/error.rs
+++ src/error.rs
@@ -17,32 +17,40 @@ pub enum Error {
     /// Error that can occur when the username cannot be obtained after registering a user.
     #[error("Failed to get username")]
     GetUsername,
+
     /// Error that can occur when the identifier of a newly created resource cannot be obtained.
     #[error("Failed to get identifier of created resource")]
     GetCreatedId,
+
     /// Error that can occur while converting a string to a date.
-    #[error("Failed to parse date: {0}")]
+    #[error("Failed to parse date")]
     ParseDate(#[from] ChronoParseError),
+
     /// Error that can occur while converting a http response into a string.
-    #[error("Failed to parse http response: {0}")]
+    #[error("Failed to parse http response")]
     ParseHttpResponse(#[from] IoError),
+
     /// Error that can occur while converting a string to an IP address.
-    #[error("Failed to parse ip address: {0}")]
+    #[error("Failed to parse ip address")]
     ParseIpAddr(#[from] AddrParseError),
+
     /// Error that can occur while parsing json content.
-    #[error("Failed to parse json content: {0}")]
+    #[error("Failed to parse json content")]
     ParseJson(#[from] SerdeJsonError),
+
     /// Error that can occur when sending HTTP requests.
-    #[error("Failed to send HTTP request: {0}")]
+    #[error("Failed to send HTTP request")]
     Request(#[from] Box<UreqError>),
+
+    #[cfg(feature = "upnp-description")]
     /// Error that can occur when deserializing [`Description`].
     ///
     /// [`Description`]: crate::bridge::Description
-    #[cfg(feature = "upnp-description")]
-    #[error("Failed to parse description: {0}")]
+    #[error("Failed to parse description")]
     ParseDescription(#[from] SerdeXmlError),
+
     /// Error that is returned by the Philips Hue API.
-    #[error("Error returned from Philips Hue API: {0}")]
+    #[error("Error returned from Philips Hue API")]
     Response(#[from] ResponseError),
 }
 

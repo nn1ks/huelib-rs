df359dde5ff85c379b74395ee26c270a10692d3b
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-08-05 18:10:30 +0200

    spelling + grammar fixes

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@
 //!
 //! - `upnp-description`: Adds support for accessing the UPnP description of a bridge. See the
 //! [`bridge::Description`] struct for more information.
-//! - `old-api`: minimal effort support for older api versions. Usefull for users of the no longer
+//! - `old-api`: Minimal effort support for older api versions. Useful for users of the no longer
 //! supported Hue v1 bridge. This lowers the supported API version to `1.16` not all features 
 //! are guarenteed to work.
 //!
